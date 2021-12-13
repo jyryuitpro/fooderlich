926aa7fb46ceb741b7b08868c3ab5c5360f23ba6
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-13 21:09:08 +0900

    Section II: Everything’s a Widget - 3.Basic Widgets - 1

---
+++ lib/circle_image.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CircleImage extends StatelessWidget {
+  final double imageRadius;
+  final ImageProvider? imageProvider;
+
+  const CircleImage({Key? key, this.imageRadius = 20, this.imageProvider}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: Colors.white,
+      radius: imageRadius,
+      child: CircleAvatar(
+        radius: imageRadius - 5,
+        backgroundImage: imageProvider,
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/card1.dart';
+import 'package:fooderlich/card2.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = [
+    const Card1(),
+    const Card2(),
+    Container(color: Colors.blue,),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Card'),
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Card2'),
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Card3'),
+        ],
+      ),
+    );
+  }
+}
